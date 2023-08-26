7419339c7273cbb899f7f88101ee402b433736d4
lovinaj <lovejon218@.com>
2023-08-25 19:53:54 -0700

    added two files

--- atoi.c
+++ atoi.c
@@ -13,19 +13,21 @@ int _atoi(char *s)
 
 	for (i = 0; s[i] != '\0' && flag != 2; i++)
 	{
-		if (s1[i] == '-')
+		if (s[i] == '-')
 			sign += -1;
 		if (s[i] >= '0' && s[i] <= '9')
 		{
 			flag = 1;
 			result *= 10;
-			result += (s[i] - '0')
+			result += (s[i] - '0');
 		}
 		else if (flag == 1)
 			flag = 2;
-		if (sign == -1)
-			outpt = -result;
-		else
-			output = result;
-		return (output);
-}
+	}	
+	if (sign == -1)
+		output = -result;
+	else
+		output = result;
+	return (output);
+	
+}	

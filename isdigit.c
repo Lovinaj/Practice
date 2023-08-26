11871bff470f8cbb6ffda1c08ab105c977252bb6
Twin J <junohanyere@gmail.com>
2023-08-26 07:13:59 +0100

    exit status

--- isdigit.c
+++ isdigit.c
@@ -1,20 +1,5 @@
-<<<<<<< HEAD
 #include "shell.h"
 
-/**
- * _isdigit - checks for digit
- * @c: parameter to be checked
- * Return: 1 for digit and 0 for other
- */
-
-int _isdigit(int c)
-{
-	if (c >= 48 && c <= 57)
-	{
-		return (1);
-	}
-	return (0);
-=======
 /**
  * _isdigit - checks if input is digit between 0 - 9
  *
@@ -30,5 +15,4 @@ int _isdigit(int c)
 		return (1);
 	else
 		return (0);
->>>>>>> 4514e84 (exit status)
 }
